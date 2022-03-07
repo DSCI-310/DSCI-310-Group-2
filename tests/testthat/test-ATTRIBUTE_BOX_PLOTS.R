c324bfec7af953a8bcc4b77fc4916bc21b11bd68
Niki Zamani <niki.zamco@gmail.com>
2022-03-07 15:12:37 -0800

    Added tests
    for the attribute_box_plots function

---
+++ tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
@@ -0,0 +1,22 @@
+library(testthat)
+source("../R/attribute_box_plots.r")
+
+testthat("'attribute_box_plots' should return at least one box plot", {
+    #tests to be added
+})
+
+testthat("'attribute_box_plots' should return a box plots for every column in the given 'data_frame' vs. the given a constant column'class_col'", {
+    #tests to be added
+})
+
+testthat("'attribute_box_plots' should return an error when incorrect data types are passes to 'data_frame' and 'class_col' arguments", {
+    #tests to be added
+})
+
+testthat("'attribute_box_plots' should return an error when the given 'data_frame' is empty", {
+    #tests to be added
+})
+
+
+
+    
