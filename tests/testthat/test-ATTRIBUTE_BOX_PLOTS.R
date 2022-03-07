564292b2ec3333d06d33ed35fb0ec879542fc0a1
Niki Zamani <niki.zamco@gmail.com>
2022-03-07 15:39:03 -0800

    fixed the test_thats
    for the attribute_box_plots function

--- tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
+++ tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
@@ -1,18 +1,19 @@
 library(testthat)
+#source ("../R/attribute_box_plots.r")
 
-testthat("'attribute_box_plots' should return at least one box plot", {
+test_that("'attribute_box_plots' should return at least one box plot",{
     #tests to be added
 })
 
-testthat("'attribute_box_plots' should return a box plots for every column in the given 'data_frame' vs. the given a constant column'class_col'", {
+test_that("'attribute_box_plots' should return a box plots for every column in the given 'data_frame' vs. the given a constant column'class_col'",{
     #tests to be added
 })
 
-testthat("'attribute_box_plots' should return an error when incorrect data types are passes to 'data_frame' and 'class_col' arguments", {
+test_that("'attribute_box_plots' should return an error when incorrect data types are passes to 'data_frame' and 'class_col' arguments", {
     #tests to be added
 })
 
-testthat("'attribute_box_plots' should return an error when the given 'data_frame' is empty", {
+test_that("'attribute_box_plots' should return an error when the given 'data_frame' is empty",{
     #tests to be added
 })
 
