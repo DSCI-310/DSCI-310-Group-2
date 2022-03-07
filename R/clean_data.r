42c059c7710417084079a3418be707979631fcbb
dliviya <dbilolikar@gmail.com>
2022-03-07 14:26:47 -0800

    created clean-data function and test suite

---
+++ R/clean_data.r
@@ -0,0 +1,15 @@
+#' Cleans dataframe
+#' 
+#' Finds all instances of missing data and converts it to NA
+#' Then removes all rows with NA to create a new dataframe
+#'
+#' @param data_frame A dataframe or dataframe extension
+#'
+#' @return A dataframe with no missing values
+#' @export
+#'
+#' @examples
+#' clean_data(heart_disease)
+clean_data <- function(data_frame) {
+  #returns a dataframe with no missing data
+}
---
+++ tests/testthat/test-CLEAN_DATA.r
@@ -0,0 +1,21 @@
+library(testthat)
+
+test_that("`clean_data` should return a dataframe", {
+  # tests to be added here
+})
+
+test_that("`clean_data` should return a dataframe with the same or less than number of rows from the original dataset", {
+  # tests to be added here
+})
+
+test_that("`clean_data` should return a dataframe with no missing values", {
+  # test1 data frame with no missing values should return same dataframe
+})
+
+test_that("`clean_data` should return a dataframe with no missing values", {
+  # tests to be added here
+})
+
+test_that("`clean_data` should throw an error when incorrect types are passed to `data_frame` argument", {
+  # tests to be added here
+})
