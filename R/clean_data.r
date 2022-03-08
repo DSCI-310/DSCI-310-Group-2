c3f99a696bb44812256366614c5b255f1952a92f
dliviya <dbilolikar@gmail.com>
2022-03-07 16:55:22 -0800

    created tests for clean_data and updated function

--- R/clean_data.r
+++ R/clean_data.r
@@ -3,13 +3,20 @@
 #' Finds all instances of missing data and converts it to NA
 #' Then removes all rows with NA to create a new dataframe
 #'
-#' @param data_frame A dataframe or dataframe extension
+#' @param df A dataframe or dataframe extension
 #'
 #' @return A dataframe with no missing values
 #' @export
 #'
 #' @examples
 #' clean_data(heart_disease)
-clean_data <- function(data_frame) {
-  #returns a dataframe with no missing data
+clean_data <- function(df) {
+    if (!is.data.frame(df)) {
+        stop("`df` should be a dataframe")
+    }
+    
+    na_df <- na_if(df, "?")
+    clean_df <- na.omit(na_df)
+    return(clean_df)
+        
 }
--- tests/testthat/test-CLEAN_DATA.r
+++ tests/testthat/test-CLEAN_DATA.r
@@ -1,21 +1,34 @@
 library(testthat)
 
+source("../R/clean_data.r")
+
 test_that("`clean_data` should return a dataframe", {
-  # tests to be added here
+    expect_s3_class(clean_data(empty), "data.frame")
+    expect_s3_class(clean_data(two_cols_1_qmissing), "data.frame")
+    expect_s3_class(clean_data(two_cols_2_qmissing), "data.frame")
+    expect_s3_class(clean_data(two_cols_2_qmissing_and_na), "data.frame")
+    expect_s3_class(clean_data(two_cols_2_na), "data.frame")
 })
 
 test_that("`clean_data` should return a dataframe with the same or less than number of rows from the original dataset", {
-  # tests to be added here
+    expect_equal(nrow(clean_data(empty)), nrow(empty))
+    expect_equal(nrow(clean_data(two_cols_1_qmissing)), nrow(two_cols_1_qmissing_output))
+    expect_equal(nrow(clean_data(two_cols_2_qmissing)), nrow(two_cols_2_qmissing_output))
+    expect_equal(nrow(clean_data(two_cols_2_qmissing_and_na)), nrow(two_cols_2_qmissing_and_na_output))
+    expect_equal(nrow(clean_data(two_cols_2_na)), nrow(two_cols_2_na_output))
+    
 })
 
 test_that("`clean_data` should return a dataframe with no missing values", {
-  # test1 data frame with no missing values should return same dataframe
+    expect_equal(clean_data(empty), empty)
+    expect_equal(clean_data(two_cols_1_qmissing), two_cols_1_qmissing_output)
+    expect_equal(clean_data(two_cols_2_qmissing), two_cols_2_qmissing_output)
+    expect_equal(clean_data(two_cols_2_qmissing_and_na), two_cols_2_qmissing_and_na_output)
+    expect_equal(clean_data(two_cols_2_na), two_cols_2_na_output)
 })
 
-test_that("`clean_data` should return a dataframe with no missing values", {
-  # tests to be added here
-})
 
 test_that("`clean_data` should throw an error when incorrect types are passed to `data_frame` argument", {
-  # tests to be added here
+    expect_error(clean_data(vec))
+    expect_error(clean_data(test_list))
 })
