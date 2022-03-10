c316dd9c7b848b6ca18e3dc396eea4d57eab042b
Ryan Peng <ryan8831@gmail.com>
2022-03-10 15:57:29 -0800

    function & test cases added

---
+++ tests/testthat/helper-accuracy_of_knn.r
@@ -0,0 +1,40 @@
+library(ggplot2)
+
+df0 <- data.frame(
+    neighbors = NULL,
+    mean = NULL
+)
+df2 <- data.frame(
+    neighbors = c(1:2),
+    mean = c(0.2, 0.4)
+)
+df5 <- data.frame(
+    neighbors = c(1:5),
+    mean = c(0.1,0.2,0.3,0.4,0.5)
+)
+
+empty_tibble <- as.tibble(df0)
+tibble_2_row <- as.tibble(df2)
+tibble_5_row <- as.tibble(df5)
+
+
+accuracy_empty_tibble <- ggplot(empty_tibble, aes(x = neighbors, y = mean)) +
+    geom_point() +
+    geom_line() +
+    labs(x = "Neighbors", y = "Accuracy Estimate") + 
+    theme(text = element_text(size = 12)) + 
+    ggtitle("Accuracy of K-NN Model with Neighbors")
+
+accuracy_tibble_2_row <- ggplot(tibble_2_row, aes(x = neighbors, y = mean)) +
+    geom_point() +
+    geom_line() +
+    labs(x = "Neighbors", y = "Accuracy Estimate") + 
+    theme(text = element_text(size = 12)) + 
+    ggtitle("Accuracy of K-NN Model with Neighbors")
+
+accuracy_tibble_5_row <- ggplot(tibble_5_row, aes(x = neighbors, y = mean)) +
+    geom_point() +
+    geom_line() +
+    labs(x = "Neighbors", y = "Accuracy Estimate") + 
+    theme(text = element_text(size = 12)) + 
+    ggtitle("Accuracy of K-NN Model with Neighbors")
---
+++ tests/testthat/test-accuracy_of_knn.r
@@ -0,0 +1,36 @@
+library(testthat)
+
+source("../../R/accuracy_plot.r")
+
+test_that("`accuracy_plot` should return a ggplot", {
+    expect_s3_class(accuracy_plot(empty_tibble), "ggplot")
+    expect_s3_class(accuracy_plot(tibble_2_row), "ggplot")
+    expect_s3_class(accuracy_plot(tibble_5_row), "ggplot")
+})
+
+test_that("`accuracy_plot` should return a ggplot and uses correct data", {
+    plot1 <- accuracy_plot(tibble_2_row)
+    plot2 <- accuracy_plot(tibble_5_row)
+    expect_that(tibble_2_row, equals(plot1$data))
+    expect_that(tibble_5_row, equals(plot2$data))
+})
+
+test_that("`accuracy_plot` should return a ggplot with x and y axis properly labeled", {
+    plot <- accuracy_plot(tibble_2_row)
+    expect_equivalent(plot$labels$x, "Neightors")
+    expect_equivalent(plot$labels$y, "Accuracy Estimate")
+})
+
+test_that("`accuracy_plot` should return a ggplot and plot layers match expectations", {
+    plot <- accuracy_plot(tibble_2_row)
+    expect_equivalent(class(plot$layers[[1]]$stat), "point")
+    expect_equivalent(class(plot$layers[[2]]$stat), "line")
+    expect_equivalent(plot, accuracy_tibble_2_row)
+    expect_equivalent(accuracy_plot(tibble_5_row), accuracy_tibble_5_row)
+})
+
+
+test_that("`accuracy_plot` should throw an error when incorrect types are passed to `tibble` argument", {
+    expect_error(accuracy_plot(0))
+    expect_error(accuracy_plot("abc"))
+})
