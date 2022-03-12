e3f5e874aa4592db77105954044fe8fda5efb0db
Niki Zamani <niki.zamco@gmail.com>
2022-03-12 01:52:59 -0800

    Added a test

--- tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
+++ tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
@@ -1,17 +1,25 @@
 library(testthat)
 #source ("../R/attribute_box_plots.r")
 
-test_that("'attribute_box_plots' should return at least one box plot",{
+test_that("'attribute_box_plots' should return a list with at least one box plot",{
     #tests to be added
 })
 
-test_that("'attribute_box_plots' should return a box plots for every column in the given 'data_frame' vs. the given a constant column'class_col'",{
+
+test_that("'attribute_box_plots' should return a list of box plots for every column in the given 'data_frame' vs. the given a constant column name",{
     #tests to be added
 })
 
-test_that("'attribute_box_plots' should return an error when incorrect data types are passes to 'data_frame' and 'class_col' arguments", {
+
+test_that("'attribute_box_plots' should return an error when incorrect data type is passed to 'data_frame' argument", {
     #tests to be added
 })
+
+
+test_that("'attribute_box_plots' should return an error when incorrect data types is passed to 'class_col' argument", {
+    #tests to be added
+})
+
 
 test_that("'attribute_box_plots' should return an error when the given 'data_frame' is empty",{
     #tests to be added
