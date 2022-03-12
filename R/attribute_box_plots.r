78ec8c1e77fe79367a7083c769fbbbfe55d62915
Niki Zamani <niki.zamco@gmail.com>
2022-03-12 15:11:29 -0800

    Added function

--- R/attribute_box_plots.r
+++ R/attribute_box_plots.r
@@ -4,7 +4,7 @@
 #' this function creates a box plot for every other column vs the constant column
 #’
 #’ @pram  data_frame  A data frame
-#’ @parm  class_col   unquoted column name of column containing class labels - variable which we want to remain constant
+#’ @parm  class_col   unquoted column name of column - variable which we want to remain constant
 #’
 #‘ @return  a list of box_plots 
 #‘        The constant column for all the boxplots
@@ -14,6 +14,21 @@
 #’
 #‘ @examples
 #‘  attribute_box_plots(heart_disease, diagnosis)
-attribute_box_plots<-function(data_frame, class_col) {
-    # returns box_plots
-}
+attribute_box_plots<-function(data_frame, {{col_n}}) {
+    plot_list <- c()
+    col_names <- c(colnames(data_frame))
+    if ({{col_n}}%in% col_names) {
+        constant_col <- select(data_frame, col_name)
+        constant_col_str <- toString(col_n)
+        for i in col_names {
+            col_str <- toString(i.colname)
+            curr_boxplot <- boxplot(i ~ col_names,
+                            data=data_frame,
+                            main=append("Figure:",col_str, "vs", constant_col_str),
+                            xlab= constant_col_str,
+                            ylab=col_str,
+                            col="orange",
+                            border="brown")
+            append(plot_list, curr_boxplot)     
+            }
+        }}
