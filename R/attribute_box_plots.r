09d7df2bcf5bbd3dd5a0d595c8ac423f7102874a
Niki Zamani <niki.zamco@gmail.com>
2022-03-12 04:33:06 -0800

    deleted the dummy file

---
+++ R/attribute_box_plots.r
@@ -0,0 +1,19 @@
+#‘ box plot creating funtion
+#‘ 
+#‘ Given a data frame and a column that we want to remain constant, 
+#' this function creates a box plot for every other column vs the constant column
+#’
+#’ @pram  data_frame  A data frame
+#’ @parm  class_col   unquoted column name of column containing class labels - variable which we want to remain constant
+#’
+#‘ @return  a list of box_plots 
+#‘        The constant column for all the boxplots
+#‘        All the other columns will be on the y axis in each of the box plots
+#‘      
+#‘ @export 
+#’
+#‘ @examples
+#‘  attribute_box_plots(heart_disease, diagnosis)
+attribute_box_plots<-function(data_frame, class_col) {
+    # returns box_plots
+}
