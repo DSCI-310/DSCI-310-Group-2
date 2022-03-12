21aeb489b6829f6e987635ec3be06bb1e9758e90
Niki Zamani <niki.zamco@gmail.com>
2022-03-12 04:30:02 -0800

    Added tests

--- tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
+++ tests/testthat/test-ATTRIBUTE_BOX_PLOTS.R
@@ -1,13 +1,15 @@
 library(testthat)
-#source ("../R/attribute_box_plots.r")
+source ("../R/attribute_box_plots.r")
 
 test_that("'attribute_box_plots' should return a list with at least one box plot",{
     #tests to be added
 })
 
 
-test_that("'attribute_box_plots' should return a list of box plots for every column in the given 'data_frame' vs. the given a constant column name",{
-    #tests to be added
+test_that("'attribute_box_plots' should return a list containing box plots for every column in the given 'data_frame' vs. the given a constant column name. The total number of box plots should be the number of columns - 1",{
+    expect_equivalent(attribute_box_plots(three_c_df, diagnosis), three_plotlist_output) 
+    expect_equivalent(attribute_box_plots(two_c_df, diagnosis), two_plotlist_output) 
+    expect_equivalent(attribute_box_plots(one_c_df, diagnosis), one_plotlist_output)
 })
 
 
