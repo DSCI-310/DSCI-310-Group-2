697d8071c592b65900924624e0129b2aa91e62ea
Niki Zamani <niki.zamco@gmail.com>
2022-03-22 17:43:26 -0700

    Created a helper for the Balance check function

---
+++ tests/testthat/helper-BALANCE_CHECK.r
@@ -0,0 +1,22 @@
+# function inputs for tests
+df0 <- data.frame()
+    
+df1 <- data.frame( 
+    sex = c(30, 40, 50),
+    age = c(0, 1, 0),
+    slope = c(3, 2, 1),
+    diagnosis = c(0, 0, 1))
+
+df2 <- data.frame(
+    age = c(20, 30),
+    diagnosis = c(1, 1)
+)
+
+
+# function outputs for tests
+empty_df <- table(df0$diagnosis)
+
+tab1 <- table(df1$diagnosis, ncol=3)
+colnames(tab1) <- c('Diagnosis','Count','Percentage')
+tab <- as.table(tab1)
+ 
