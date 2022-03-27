673fc6edba87d5a6cfc59910035dad19d7c61323
Niki Zamani <niki.zamco@gmail.com>
2022-03-27 01:22:15 -0700

    Edited the function outputs and added types for the input

--- tests/testthat/helper-BALANCE_CHECK.r
+++ tests/testthat/helper-BALANCE_CHECK.r
@@ -2,21 +2,50 @@
 df0 <- data.frame()
     
 df1 <- data.frame( 
-    sex = c(30, 40, 50),
-    age = c(0, 1, 0),
-    slope = c(3, 2, 1),
-    diagnosis = c(0, 0, 1))
+    sex = c(factor(30), factor(40), factor(50)),
+    age = c(double(0), double(1), double(0)),
+    slope = c(factor(3), factor(2), factor(1)),
+    diagnosis = c(factor(0), factor(0), factor(1)))
 
 df2 <- data.frame(
-    age = c(20, 30),
-    diagnosis = c(1, 1)
+    age = c(double(20), double(30)),
+    diagnosis = c(factor(1), factor(1))
 )
 
 
 # function outputs for tests
 empty_df <- table(df0$diagnosis)
 
-tab1 <- table(df1$diagnosis)
+num_obs <- nrow(df1)
+tab1_output <- group_by(df1, "diagnosis") %>%
+                                summarize(count = n(),
+                                          percentage = n() / num_obs * 100)
+tab1_output
 
-tab <- as.table(tab1)
+
+
+
+num_obs <- nrow(df2)
+tab2_output <- group_by(df2, "diagnosis") %>%
+                                summarize(count = n(),
+                                          percentage = n() / (num_obs * 100))
+tab2_output
+
+
+
+
+
+tab2_output <- matrix(c (0, as.integer(0), as.double(0), 1, as.integer(2), as.double(100)), 
+              ncol=3, 
+              byrow=TRUE)
+colnames(tab2_output) <- c('diagnosis','count','percentage')
+tab2_output <- as.table(tab2_output)
+
+
+
+
+
+
+
+
  
