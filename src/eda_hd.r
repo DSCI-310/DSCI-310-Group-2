55c49fb1ec4a287c76c17e7941bac98c06639939
Emily <eahn01@students.cs.ubc.ca>
2022-03-28 21:41:43 +0000

    Added pie plot and correlation plot

---
+++ src/eda_hd.r
@@ -0,0 +1,37 @@
+"Creates eda plots for the pre-processed data from the Preprocessed cleveland heart disease data (from https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data).
+Saves the plots as pdf and pnt file.
+Usage: src/eda_hd.r --url=<url> --out_file=<out_file>
+
+Options:
+--url=<url>              URL from where to download the data (must be in standard csv format)
+--out_file=<out_file>    Path (including filename) of where to locally write the file
+" -> doc
+
+library(feather)
+library(tidyverse)
+library(caret)
+library(docopt)
+library(ggridges)
+library(ggthemes)
+theme_set(theme_minimal())
+
+opt <- docopt(doc)
+
+main <- function(train, out_dir) {
+    # generate pie plot to visualize distribution of diagnosis
+    pie_distribution <- pie(table(train$diagnosis), main = "Figure 3: Distribution of Diagnosis", col = c("Blue", "Red"))
+    ggsave(paste0(out_dir, "/Distribution_of_diagnosis.png"), 
+         pie_distribution,
+         width = 8, 
+         height = 10)
+    
+    # generate correlation plot
+    data_correlation <- ggcorr(heart_disease_numerical, label = TRUE, label_size = 4, label_round = 2, label_alpha = FALSE) +
+                        ggplot2::labs(title = "Figure 4: Correlation between Variables")
+    ggsave(paste0(out_dir, "/varaible_correlation.png"), 
+         data_correlation,
+         width = 8, 
+         height = 10)
+        
+    
+}
