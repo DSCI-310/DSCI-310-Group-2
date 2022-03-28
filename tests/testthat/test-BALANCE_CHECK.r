c2d25d71d4321ad8535f3bf7abf8720c165dc848
Niki Zamani <niki.zamco@gmail.com>
2022-03-28 14:01:15 -0700

    addded a test

--- tests/testthat/test-BALANCE_CHECK.r
+++ tests/testthat/test-BALANCE_CHECK.r
@@ -2,6 +2,12 @@ library(testthat)
 source("../../R/balance_check.r")
 source("../../tests/testthat/helper-BALANCE_CHECK.R")
 
+test_that("'balance_check' should return a table", {
+    expect_type(tab1_output, "list")
+    expect_type(tab2_output, "list")
+})
+
+    
 test_that("'balance_check' should return a table with the count and percentage based on the given column's data.",{
     expect_equivalent(balance_check(df1, "diagnosis"), tab1_output) 
     expect_equivalent(balance_check(df2, "diagnosis"), tab2_output) 
@@ -9,10 +15,14 @@ test_that("'balance_check' should return a table with the count and percentage b
 
 test_that("'balance_check' should return an error when incorrect data types or unrecognizable column is passed to 'class_col' argument", {
     expect_error(balance_check(df0, data_ls)) 
-    expect_error(balance_check(df1, "slope")) 
+    expect_error(balance_check(df2, "slope")) 
 })
 
 
 test_that("'balance_check' should return an error when the given 'data_frame' is empty",{
     expect_error(balance_check(df0, "diagnosis")) 
 })
+
+
+#ask quotes aroung the col_name
+#ask about the tibble / list / dataframe
