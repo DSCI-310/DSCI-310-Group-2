ed3ef4069ccd7e38fae3b97cf6444e024bfc4659
Emily <eahn01@students.cs.ubc.ca>
2022-03-28 21:58:17 +0000

    Added box plot

--- src/eda_hd.r
+++ src/eda_hd.r
@@ -1,11 +1,10 @@
-"Creates eda plots for the pre-processed training data from Preprocessed cleveland heart disease data (from https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data).
-Saves the plots as a pdf and png file.
-Usage: src/eda_wisc.r --train=<train> --out_dir=<out_dir>
-  
+"Creates eda plots for the pre-processed data from the Preprocessed cleveland heart disease data (from https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data).
+Saves the plots as pdf and pnt file.
+Usage: src/eda_hd.r --url=<url> --out_file=<out_file>
+
 Options:
---full=<full>     Path (including filename) to raw data
---train=<train>     Path (including filename) to training data 
---out_dir=<out_dir> Path to directory where the plots should be saved
+--url=<url>              URL from where to download the data (must be in standard csv format)
+--out_file=<out_file>    Path (including filename) of where to locally write the file
 " -> doc
 
 library(feather)
@@ -18,18 +17,24 @@ theme_set(theme_minimal())
 
 opt <- docopt(doc)
 
-main <- function(full, train, out_dir) {
-    train_data <- read_csv(train)
-    full_data <- read_csv(full)
+main <- function(train, out_dir) {
+    # generate box plot
+    box_hd <- attribute_box_plots(train, diagnosis)
+    ggsave(paste0(out_dir, "/box_hd.png"), 
+         pie_distribution,
+         width = 8, 
+         height = 10)
+    
     # generate pie plot to visualize distribution of diagnosis
-    pie_distribution <- pie(table(raw_data$diagnosis), col = c("Blue", "Red"))
+    pie_distribution <- pie(table(train$diagnosis), main = "Figure 3: Distribution of Diagnosis", col = c("Blue", "Red"))
     ggsave(paste0(out_dir, "/Distribution_of_diagnosis.png"), 
          pie_distribution,
          width = 8, 
          height = 10)
     
     # generate correlation plot
-    data_correlation <- ggcorr(full_data, label = TRUE, label_size = 4, label_round = 2, label_alpha = FALSE)
+    data_correlation <- ggcorr(heart_disease_numerical, label = TRUE, label_size = 4, label_round = 2, label_alpha = FALSE) +
+                        ggplot2::labs(title = "Figure 4: Correlation between Variables")
     ggsave(paste0(out_dir, "/varaible_correlation.png"), 
          data_correlation,
          width = 8, 
