75dd1e93807c6cde1c8d39fbb6a0f0e3ba2b0186
Divya <dbilolikar@gmail.com>
2022-03-29 22:55:53 +0000

    finished changes to third script and respective makefile changes

--- src/eda_hd.r
+++ src/eda_hd.r
@@ -1,10 +1,10 @@
 "Creates eda plots for the pre-processed data from the Preprocessed cleveland heart disease data (from https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data).
 Saves the plots as pdf and pnt file.
-Usage: src/eda_hd.r --url=<url> --out_file=<out_file>
+Usage: src/eda_hd.r --full=<full> --out_dir=<out_dir>
 
 Options:
---url=<url>              URL from where to download the data (must be in standard csv format)
---out_file=<out_file>    Path (including filename) of where to locally write the file
+--full=<full>     Path (including filename) to training data (must be a csv)
+--out_dir=<out_dir> Path to directory where the plots should be saved
 " -> doc
 
 library(feather)
@@ -17,28 +17,31 @@ theme_set(theme_minimal())
 
 opt <- docopt(doc)
 
-main <- function(train, out_dir) {
+main <- function(full, out_dir) {
+    
+    data <- read_csv(full)
     # generate box plot
-    box_hd <- attribute_box_plots(train, diagnosis)
+    box_hd <- attribute_box_plots(data, diagnosis)
     ggsave(paste0(out_dir, "/box_hd.png"), 
          pie_distribution,
          width = 8, 
          height = 10)
     
     # generate pie plot to visualize distribution of diagnosis
-    pie_distribution <- pie(table(train$diagnosis), main = "Figure 3: Distribution of Diagnosis", col = c("Blue", "Red"))
+    pie_distribution <- pie(table(data$diagnosis), main = "Figure 3: Distribution of Diagnosis", col = c("Blue", "Red"))
     ggsave(paste0(out_dir, "/Distribution_of_diagnosis.png"), 
          pie_distribution,
          width = 8, 
          height = 10)
     
     # generate correlation plot
-    data_correlation <- ggcorr(heart_disease_numerical, label = TRUE, label_size = 4, label_round = 2, label_alpha = FALSE) +
+    data_correlation <- ggcorr(data, label = TRUE, label_size = 4, label_round = 2, label_alpha = FALSE) +
                         ggplot2::labs(title = "Figure 4: Correlation between Variables")
     ggsave(paste0(out_dir, "/varaible_correlation.png"), 
          data_correlation,
          width = 8, 
          height = 10)
         
-    
 }
+
+main(opt[["--full"]], opt[["--out_dir"]])
