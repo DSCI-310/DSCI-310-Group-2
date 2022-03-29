d45623d1da86bc50f8a0f308360e81b82fa84d62
Niki Zamani <niki.zamco@gmail.com>
2022-03-29 15:46:47 -0700

    deleted one test

--- tests/testthat/test-BALANCE_CHECK.r
+++ tests/testthat/test-BALANCE_CHECK.r
@@ -9,18 +9,13 @@ test_that("'balance_check' should return a table", {
 
     
 test_that("'balance_check' should return a table with the count and percentage based on the given column's data.",{
-    expect_equivalent(balance_check(df1, "diagnosis"), tab1_output) 
-    expect_equivalent(balance_check(df2, "diagnosis"), tab2_output) 
-})
-
-test_that("'balance_check' should return an error when incorrect data types or unrecognizable column is passed to 'class_col' argument", {
-    expect_error(balance_check(df0, data_ls)) 
-    expect_error(balance_check(df2, "slope")) 
+    expect_equivalent(balance_check(df1, diagnosis), tab1_output) 
+    expect_equivalent(balance_check(df2, diagnosis), tab2_output) 
 })
 
 
 test_that("'balance_check' should return an error when the given 'data_frame' is empty",{
-    expect_error(balance_check(df0, "diagnosis")) 
+    expect_error(balance_check(df0, diagnosis)) 
 })
 
 
