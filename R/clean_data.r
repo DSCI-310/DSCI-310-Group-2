2c76348cc76f0796b11b467f50336821d63bdaae
Niki Zamani <niki.zamco@gmail.com>
2022-03-29 17:49:39 -0700

    changed indentation

--- R/clean_data.r
+++ R/clean_data.r
@@ -19,7 +19,7 @@ clean_data <- function(df) {
     na_df <- dplyr::na_if(df, "?")
     if (nrow(na_df) == 0) {
         return(na_df)
-        }
+    }
     else {
         na_df[] <- lapply(na_df, function(x) as.numeric(as.character(x)))
         clean_df <- na.omit(na_df)
