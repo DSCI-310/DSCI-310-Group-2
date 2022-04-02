e038203be3e05d8c25a7b3e30980f58f069e9144
Ryan Peng <ryan8831@gmail.com>
2022-04-02 15:01:29 -0700

    removed read_feather

--- src/model_results.r
+++ src/model_results.r
@@ -4,7 +4,7 @@ Options:
 --test=<test>          Path (including filename) to test data (which needs to be saved as a feather file)
 --out_dir=<out_dir>    Path to directory where the plots should be saved
 " -> doc
-  
+
 library(tidyverse)
 library(caret)
 library(docopt)
@@ -13,22 +13,22 @@ set.seed(2019)
 opt <- docopt(doc)
 
 main <- function(test, out_dir) {
-    
+
  # Load and wrangle test data ----------------------------------------------
-  test_data <- read_feather(test) 
-  x_test <- test_data %>% 
+  test_data <- read.csv(test)
+  x_test <- test_data %>%
     select(-diagnosis)
-  y_test <- test_data %>% 
-    select(diagnosis) %>% 
-    mutate(diagnosis = as.factor(diagnosis)) %>% 
+  y_test <- test_data %>%
+    select(diagnosis) %>%
+    mutate(diagnosis = as.factor(diagnosis)) %>%
     pull()
-  
+
   # Load model and predict --------------------------------------------------
   final_model <- readRDS("results/final_model.rds")
   y_pred <- predict(final_model, x_test)
-  
+
   # assess model accuracy ---------------------------------------------------
   model_quality <- confusionMatrix(y_pred, y_test)
   saveRDS(model_quality, file = paste0(out_dir, "/final_model_quality.rds"))
 }
-main(opt[["--test"]], opt[["--out_dir"]]) 
+main(opt[["--test"]], opt[["--out_dir"]])
